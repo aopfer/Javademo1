8a5189bc1245a216d5a32de8bd17f5b5340844cd
kngp54 <kngp54@192.168.113.110>
2017-09-20 11:02:03 -0500

    first commit

---
+++ src/javademo1/Javademo1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javademo1;
+
+/**
+ *
+ * @author kngp54
+ */
+public class Javademo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+        //psvm
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+    //sout
+    System.out.println("Hello World!");
+    }
+}
